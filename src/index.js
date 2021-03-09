5a91f983ae565710d4e55e3135c6853de26bdbfd
Kartikeya Mudgal <kartikeya.mudgal28@gmail.com>
2021-03-09 19:28:19 +0530

    jsx javascript

--- src/index.js
+++ src/index.js
@@ -14,27 +14,23 @@ function BookList() {
   );
 }
 
-const Book = () => (
-  <article className="book">
-    <Image />
-    <Title />
-    <Author />
-  </article>
-);
-const Image = () => (
-  <img
-    src="https://images-eu.ssl-images-amazon.com/images/I/81l3rZK4lnL._AC_UL200_SR200,200_.jpg"
-    alt=""
-  />
-);
+const Book = () => {
+  const title = "Ikigai: The Japanese secret to a long and happy life";
+  const author = "Héctor García";
+  return (
+    <article className="book">
+      <img
+        src="https://images-eu.ssl-images-amazon.com/images/I/81l3rZK4lnL._AC_UL200_SR200,200_.jpg"
+        alt=""
+      />
+      <h1>{title}</h1>
+      <h4>{author}</h4>
+      {}
+    </article>
+  );
+};
 
-const Title = () => (
-  <h1>Ikigai: The Japanese secret to a long and happy life</h1>
-);
-const Author = () => (
-  <h4 style={{ color: "#617d98", fontSize: "0.75rem", marginTop: "0.25rem" }}>
-    Héctor García
-  </h4>
-);
 // {} => to access JS world
+// Always pass something in {} that returns the value (an expression)
+// {let x = 6} (a statement) will generate error
 ReactDom.render(<BookList />, document.getElementById("root"));
