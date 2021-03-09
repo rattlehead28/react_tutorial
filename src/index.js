9ebada8b22eea57887193468d1fa6655b21594a8
Kartikeya Mudgal <kartikeya.mudgal28@gmail.com>
2021-03-09 20:12:25 +0530

    props-children

--- src/index.js
+++ src/index.js
@@ -22,7 +22,13 @@ function BookList() {
         img={firstBook.img}
         title={firstBook.title}
         author={firstBook.author}
-      />
+      >
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quod aperiam
+          doloribus a perspiciatis odio! Delectus consequatur laboriosam
+          reprehenderit ex earum.
+        </p>
+      </Book>
       <Book
         img={secondBook.img}
         title={secondBook.title}
@@ -32,14 +38,14 @@ function BookList() {
   );
 }
 
-const Book = ({ img, title, author }) => {
+const Book = ({ img, title, author, children }) => {
   // const { img, title, author } = props;
   return (
     <article className="book">
       <img src={img} alt="" />
       <h1>{title}</h1>
       <h4>{author}</h4>
-      {}
+      {children}
     </article>
   );
 };
